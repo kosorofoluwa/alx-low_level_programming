ff2ef2d2d18c5dd6dfd293ab9f042a3a1f198d98
kosorofoluwa <kosorofoluwaararomi@gmail.com>
2023-06-21 01:51:29 -0700

    Hello puts Hello printf Size not grandeur

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Prints a program followed by a new line
+ *
+ * Return: 0 Always
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
