9b7d54d0d3bdfa162cf719fbf05e556a2d1e6b09
kosorofoluwa <kosorofoluwaararomi@gmail.com>
2023-07-13 10:03:44 -0700

    correction

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,10 +4,10 @@
 /**
  * main - prints a program followed by a new line
  *
- * Return: 0 Always
+ * Return: return 1
  */
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	return (0);
+	return (1);
 }
