65a5672ac2dda7eb7562bf1fb6d1eb88effcd08f
kosorofoluwa <kosorofoluwaararomi@gmail.com>
2023-07-17 14:50:11 -0700

    correction

--- 0x0D-preprocessor/02-main.c
+++ 0x0D-preprocessor/02-main.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 /**
- * main - prints the name of the file it was compiled from,
- * followed by a new line.
+ * main - prints the name of the file from which it was compiled
+ *
  * Return 0 always
  */
 int main(void)
